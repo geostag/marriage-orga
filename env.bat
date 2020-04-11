b893f893af49e054cdaf2907c57e5c6de20c4c22
Dr. Georg Ostertag <georg.ostertag@muenchen.de>
2020-04-11 16:39:43 +0200

    init 0

---
+++ env.bat
@@ -0,0 +1,26 @@
+@echo off
+
+rem forfiles /P . /S /D +08.10.2019 /c "cmd /c echo @path"
+
+echo setting up env
+
+rem with http (NOT https) as a proxy, git-gui works
+rem set HTTPS_PROXY=http://10.158.0.79:80/
+set path=%USERPROFILE%\AppData\Local\Programs\Python\Python37\Scripts\;C:\Users\georg.ostertag\AppData\Local\Programs\Python\Python37\;%path%
+
+set appbasedir=c:\var\workplace\marriage-orga
+
+set DB_ENGINE=django.db.backends.sqlite3
+set DB_NAME=%appbasedir%/runtime/django-db.sqlite3
+set RELEASE="dev"
+set SECRET_KEY="_)9u76xogosaka&$5)0ertz9iv)h56kje@49rsc)2igi=y65o$"
+set SW_HEADER_INSTANCE=Full
+set INSTANCE=DEV
+
+set APP_PORT=8001
+
+set DEBUG=DEBUG
+
+set dj_basedir=%appbasedir%
+set dj_runtimedir=%dj_basedir%\runtime
+
---
+++ env.sh
@@ -0,0 +1,77 @@
+#!/bin/bash 
+
+export appname=jstammbaum
+export dj_basedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export appdir="${dj_basedir}/app"
+export dj_runtimedir="${dj_basedir}/runtime"
+export dj_pyenvdir="${dj_runtimedir}/penv"
+
+# set up python virtualenv
+if [ -r $dj_pyenvdir/bin/activate ]; then
+    . $dj_pyenvdir/bin/activate
+else
+    echo "pyenv not there. You might need to run ${dj_basedir}/tools/build-pyenv.sh first"
+fi
+
+export HOSTNAME=`uname -n`
+
+# set up django specific env
+# override these in your private dj_-db-env.sh
+export DB_ENGINE=django.db.backends.postgresql
+export DB_NAME=stammbaum
+export DB_USER=stammbaum
+export DB_PASSWORD=stammbaump
+export DB_HOST=localhost
+export DB_PORT=5432
+
+if [ `uname -n` = "snoopy" ]; then
+    INSTANCE="DEV"
+elif [ `uname -n` = "genf031" ]; then
+    INSTANCE=""
+else
+    INSTANCE="TEST"
+fi
+
+export INSTANCE
+
+if [ -r $dj_basedir/RELEASE ]; then
+    export RELEASE=`cat $dj_basedir/RELEASE`
+else
+    export RELEASE="dev-release"
+fi
+
+export APP_PORT=8001
+
+# get private settings
+if [ -r $dj_basedir/${appname}-db-env.sh ]; then
+    . $dj_basedir/${appname}-db-env.sh
+elif [ -r $HOME/.${appname}-db-env.sh ]; then
+    . $HOME/.${appname}-db-env.sh
+else
+    echo "ya need $HOME/.${appname}-db-env.sh or $dj_basedir/${appname}-db-env.sh"
+fi
+
+if [ "_$DB_ENGINE" = "_django.db.backends.postgresql" ]; then
+    export PGPORT=$DB_PORT
+    export PGUSER="$DB_USER"
+    export PGPASSWORD="$DB_PASSWORD"
+    export PGDATABASE="$DB_NAME"
+    export PGHOST=$DB_HOST
+fi
+
+# aliases
+alias ${appname}-updatepythonrequirements="pip install -r ${appdir}/requirements.txt)"
+
+alias ${appname}-test="$dj_basedir/tools/django-test.pl"
+alias ${appname}-startup="(cd $abbdir && ./run.sh)"
+alias ${appname}-runtestserver="(cd $appdir && python manage.py runserver 0.0.0.0:8001)"
+alias ${appname}-shell="(cd $appdir && python manage.py shell)"
+
+alias ${appname}-collectstatic="(cd $appdir && python manage.py collectstatic --noinput)"
+alias ${appname}-migrate="(cd $appdir && python manage.py migrate)"
+alias ${appname}-createcachetable="(cd $appdir && python manage.py createcachetable)"
+alias ${appname}-initialize="${appname}-collectstatic ;${appname}-migrate ;${appname}-createcachetable"
+
+alias ${appname}-deploy="(cd $dj_basedir && tools/deploy-prod)"
+#alias ${appname}-deploy-migrate="sudo -u www-data bash -c 'cd /srv/www/dj_/django/2.1 && svn up && . env.sh && cd dj_ && python manage.py collectstatic --noinput && python manage.py migrate && ./run.sh'"
+
